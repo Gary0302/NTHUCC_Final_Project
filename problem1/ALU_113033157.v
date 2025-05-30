75ded6111449925c53f61bdb4b2d818fb892ce22
blackmoonlin <blackmoon941019@gmail.com>
2025-05-30 16:00:23 +0800

    Update ALU_113033157.v

--- problem1/ALU_113033157.v
+++ problem1/ALU_113033157.v
@@ -9,17 +9,17 @@ module ALU (
 
     always @(*) begin
         case (instruction)
-            4'b0000: F = A + B;                                    // 無符號加法
-            4'b0001: F = A - B;                                    // 無符號減法
-            4'b0010: F = A >> B;                                   // 邏輯右移
-            4'b0011: F = A << B;                                   // 邏輯左移
-            4'b0100: F = (A >> B) | (A << (8 - B));               // 右旋轉
-            4'b0101: F = (A << B) | (A >> (8 - B));               // 左旋轉
-            4'b0110: F = A & B;                                    // AND
-            4'b0111: F = A | B;                                    // OR
-            4'b1000: F = ~A;                                       // NOT (B未使用)
-            4'b1001: F = A ^ B;                                    // XOR
-            default: F = 8'h00;                                    // 預設值
+            4'b0000: F = A + B;                                    
+            4'b0001: F = A - B;                                    
+            4'b0010: F = A >> B;                                   
+            4'b0011: F = A << B;                                   
+            4'b0100: F = (A >> B) | (A << (8 - B));               
+            4'b0101: F = (A << B) | (A >> (8 - B));               
+            4'b0110: F = A & B;                                    
+            4'b0111: F = A | B;                                    
+            4'b1000: F = ~A;                                       
+            4'b1001: F = A ^ B;                                    
+            default: F = 8'h00;                                    
         endcase
     end
 
